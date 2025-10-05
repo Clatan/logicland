078d685145e4f26fcbde82e832ea9edeb3d03623
Clatan <claribelaurel30@gmail.com>
2025-10-05 21:52:19 +0700

    load page

---
+++ resources/views/load.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logic Land</title>
+    <link rel="icon" type="svg+xml" href="{{ asset('asset/logo.svg') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jolly+Lodger&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="centered-container">
+        <a href="{{ url('/') }}" class="start-button">
+            <h1 class="text-overlay">Start</h1>
+        </a>
+        <img src="{{ asset('asset/logo.svg') }}" alt="Logic Land" style="position:absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); position: absolute; z-index: 5;">            
+    </div>
+</body>
+
+<style>
+    body {
+        background: url('{{ asset('asset/bckg.svg') }}');
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+</html>
